016ee7e3eceac1c7bbd1f8078410c92f312af65d
zero <zero@example.com>
2023-04-25 09:02:36 +0900

    Create a new registration and login function

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list, name='post_list'),
+    path('', views.base, name='base'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_list(request):
-  return render(request,'blog/post_list.html',{})
+def base(request):
+  return render(request,'base.html')
+
+def signup(request):
+  return render(request,'signup.html')
+
+def login(request):
+  return render(request,'login.html')
